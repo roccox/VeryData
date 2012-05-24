d68ff354e313a3f387bb941135b3e42f40da4efb
Rock.Jin <roc.bie@gmail.com>
2012-05-24 17:00:07 +0800

    minor bugs.

--- VeryData/SentViewController.m
+++ VeryData/SentViewController.m
@@ -195,7 +195,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    [self configureView];
+//    [self configureView];
 }
 
 - (void)viewDidUnload
